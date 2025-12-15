54696fd2b034df1b4f911651a5898186ba668540
Snehasis <snehasiskoley95@gmail.com>
2025-12-15 17:28:47 +0530

    eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-export default app; 
+export default app;
