0dd7383e5b61c4ee925621f04f8b041f299f5018
Snehasis <snehasiskoley95@gmail.com>
2025-12-15 18:16:29 +0530

    loggerand winston

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import logger from './config/logger';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 const app = express();
+    
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: { write: msg => logger.info(msg.trim()) } }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
+    logger.info('Yoo... endpoint was hit');
   res.send('Hello, World!');
 });
 
